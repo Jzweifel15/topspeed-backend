0dc345f80542a0adcc9221928a3386c21826281e
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-11 15:11:39 -0500

    Added to Drivers
    added more attributes to drivers for later possible authentication

--- db/migrate/20201211190740_create_drivers.rb
+++ db/migrate/20201211190740_create_drivers.rb
@@ -1,6 +1,10 @@
 class CreateDrivers < ActiveRecord::Migration[6.0]
   def change
     create_table :drivers do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2020_12_11_190740) do
   end
 
   create_table "drivers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
